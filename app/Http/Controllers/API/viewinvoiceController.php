6d1442a1f880c08da2bb1b2f99f190127d8b2ad6
npkarunarathne <npkarunarathne@gmail.com>
2019-06-20 21:35:48 +0530

    payments

---
+++ app/Http/Controllers/API/viewinvoiceController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use DB;
+class viewinvoiceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $out = json_decode($request->getContent('dataI'), true);
+        $formData = $out['dataI'];
+
+        foreach ($formData as $inv) {
+            foreach ($inv as $c) {
+                if(empty($c['amount'])){
+                    $c['amount']=0;
+                }
+                DB::update('update outstanding__payments set SettledAmount = ? , Balance = ? where InvoiceNo=?',[$c['SettledAmount'] +$c['amount'], $c['Balance']-$c['amount'] , $c['InvoiceNo']]);
+                //return $c['Balance']-$c['amount'];
+            }
+        } 
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $invoices = DB::table('outstanding__payments')->where('CustomerID', $id)->get();
+        return $invoices;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- public/js/app.js
+++ public/js/app.js
@@ -64620,8 +64620,8 @@ $(document).ready(function () {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! E:\project 2 vids\sonsy new\ERP_for_Sonsy\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! E:\project 2 vids\sonsy new\ERP_for_Sonsy\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\Project_2\sonsy\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\Project_2\sonsy\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
